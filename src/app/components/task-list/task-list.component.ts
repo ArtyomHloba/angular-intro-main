75ea75b926e30d2d7f270ddb48636505c72991d8
ArtyomHloba <artemgloba2@gmail.com>
2025-03-13 11:02:26 +0200

    Fix methods for tasks

--- src/app/components/task-list/task-list.component.ts
+++ src/app/components/task-list/task-list.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core'
 import { Task } from '../../core/models/task.model'
 import { tasks } from '../../core/moc_data/tasks'
 import { TaskStatus } from '../../core/models/status.enum'
+import { TaskService } from '../../services/task.service'
 
 @Component({
   selector: 'app-task-list',
@@ -11,40 +12,36 @@ import { TaskStatus } from '../../core/models/status.enum'
 })
 export class TaskListComponent {
   myTasks: Task[] = tasks
-  TaskStatus = TaskStatus
+
+  protected readonly TaskStatus = TaskStatus
   selectedStatus: TaskStatus | 'all' = 'all'
 
-  deleteTask (index: number): void {
-    this.myTasks = this.myTasks.filter(task => task.id !== index)
+  editingTask: Task | null = null
+
+  constructor (private taskService: TaskService) {}
+
+  ngOnInit (): void {
+    this.loadTasks()
   }
 
-  onSelected (event: Event): void {
-    const status = (event.target as HTMLSelectElement).value
-    this.selectedStatus = status as TaskStatus | 'all'
+  loadTasks (): void {
+    this.myTasks = this.taskService.getTasks()
+  }
+
+  deleteTask (index: number): void {
+    this.taskService.deleteTask(index)
+    this.loadTasks()
   }
 
   addTask (task: Task): void {
     if (this.editingTask) {
-      this.myTasks = this.myTasks.map((t: Task) =>
-        t.id === task.id ? { ...task } : t
-      )
+      this.taskService.updateTask(task)
       this.editingTask = null
     } else {
-      const maxId: number =
-        this.myTasks.length > 0
-          ? Math.max(...this.myTasks.map((task: Task) => task.id))
-          : 0
-
-      task = {
-        ...task,
-        id: maxId + 1
-      }
-
-      this.myTasks.push(task)
+      this.taskService.addTask(task)
     }
+    this.loadTasks()
   }
-
-  editingTask: Task | null = null
 
   editTask (task: Task): void {
     this.editingTask = { ...task }
@@ -52,5 +49,10 @@ export class TaskListComponent {
 
   trackTask (index: number, task: Task) {
     return task.id
+  }
+
+  onSelected (event: Event): void {
+    const status = (event.target as HTMLSelectElement).value
+    this.selectedStatus = status as TaskStatus | 'all'
   }
 }
